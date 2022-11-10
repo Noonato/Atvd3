1c87a141b78fc394b6cd98f1be8217d295d348dc
Noonato <101616181+Noonato@users.noreply.github.com>
2022-11-09 21:51:45 -0300

    Delete ATVD1.py

--- ATVD1.py
+++
@@ -1,7 +0,0 @@
-idade = int(input('Informe sua idade em dias: '))
-
-anos = int(idade / 365)
-meses = int(idade %365)/30
-dias = (idade % 365)%30
-
-print(f'Anos:{anos}\nMeses:{meses}\nDias:{dias}')
